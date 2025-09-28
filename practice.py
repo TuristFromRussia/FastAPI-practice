268f3ce57b5b2385cf0d090fb25b9d7bce0020d7
Ilya Efimov <vasanovna91@gmail.com>
2025-09-28 07:57:44 +0300

    second step, last on this day

--- practice.py
+++ practice.py
@@ -1,2 +1,17 @@
 from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+def say_hui():
+    return 'huilo'
+
+@app.get("/minus")
+def minus_two(a: int, b: int) -> int:
+    return a - b
+
+@app.get("/fuck/{print}")
+def paint(print: str):
+    ret = ''
+    ret = print.upper()
+    return ret
