f0f64478b63ddfc1be76b6e68f6591a4847f12d3
Ilya Efimov <vasanovna91@gmail.com>
2025-10-03 00:54:51 +0300

    next step of my learning

--- practice.py
+++ practice.py
@@ -1,4 +1,25 @@
+import datetime
+from typing import List
+from loguru import logger
 from fastapi import FastAPI
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from pydantic import BaseModel
+
+class MembersGet(BaseModel):
+    id: int
+    surname: str
+    firstname: str
+    address: str
+    zipcode: int
+    telephone: str
+    recommendedby: int | None
+    joindate: datetime.datetime
+
+    model_config = {
+        "from_attributes": True 
+    }
+
 
 app = FastAPI()
 
@@ -15,3 +36,37 @@ def paint(print: str):
     ret = ''
     ret = print.upper()
     return ret
+
+@app.post("/user")
+def user(name):
+    return {"message": f"hello, {name}"}
+
+
+@app.get("/members/all", response_model=List[MembersGet])
+def all_members():
+    conn = psycopg2.connect("postgresql://postgres:valdorf2015@localhost:5432/exercises?client_encoding=WIN1252", cursor_factory=RealDictCursor)
+    cursor = conn.cursor()
+    cursor.execute("""
+    SELECT memid AS id,
+        surname,
+        firstname, 
+        address,
+        zipcode, 
+        telephone, 
+        recommendedby, 
+        joindate
+    FROM cd.members
+    """)
+    results = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return results
+
+
+
+
+
+
+
+# python -m uvicorn practice:app --reload
+# запуск локального сервера
